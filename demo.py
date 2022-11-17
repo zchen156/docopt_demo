0a27e0e51b80942f35393623907b1fbcc9ee6994
zchen156 <112703022+zchen156@users.noreply.github.com>
2022-11-17 11:10:20 -0800

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,16 @@
+# author: Tiffany Timbers
+# date: 2020-01-15
+
+"""This script prints out docopt args.
+Usage: demo.py <arg1> --arg2=<arg2> [--arg3=<arg3>]
+
+Options:
+<arg1>             Takes any value (this is a required positional argument)
+--arg2=<arg2>     Takes any value (this is a required option)
+[--arg3=<arg3>]   Takes any value (this is an optional option)
+""" 
+
+from docopt import docopt
+opt = docopt(__doc__)
+print(opt)
+print(type(opt))
