9a66ede06b9e07012dde8476d96568d6230819f8
zchen156 <112703022+zchen156@users.noreply.github.com>
2022-11-19 11:04:25 -0800

    Exercise 5

--- demo.R
+++ demo.R
@@ -2,12 +2,13 @@
 # date: 2020-01-15
 
 "This script prints out docopt args.
-Usage: demo.R <arg1> --arg2=<arg2> [--arg3=<arg3>]
+Usage: demo.R <arg1> --arg2=<arg2> [--arg3=<arg3>][<arg4>]
 
 Options:
 <arg>             Takes any value (this is a required positional argument)
 --arg2=<arg2>     Takes any value (this is a required option)
 [--arg3=<arg3>]   Takes any value (this is an optional option)
+[<arg4>]          Takes any value (optional positional argument)
 " -> doc
 
 library(docopt)
