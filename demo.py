78b2ce7b6eed9adf7cf2186aa392a86493fa8777
zchen156 <112703022+zchen156@users.noreply.github.com>
2022-11-19 11:17:55 -0800

    Exercise 8

--- demo.py
+++ demo.py
@@ -2,12 +2,13 @@
 # date: 2020-01-15
 
 """This script prints out docopt args.
-Usage: demo.py <arg1> --arg2=<arg2> [--arg3=<arg3>]
+Usage: demo.py <arg1> --arg2=<arg2> [--arg3=<arg3>] [<arg4>] 
 
 Options:
 <arg1>             Takes any value (this is a required positional argument)
 --arg2=<arg2>     Takes any value (this is a required option)
 [--arg3=<arg3>]   Takes any value (this is an optional option)
+[<arg4>]          Takes any value (this is an optional positional argument)
 """ 
 
 from docopt import docopt
